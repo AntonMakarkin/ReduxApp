3738c8c353afd04e5fb80bebe3d4aa90f9b1343c
AntonMakarkin <amakarkin27@gmail.com>
2020-11-07 00:26:58 +0300

    Lesson 107. React + Redux

---
+++ src/actions.js
@@ -0,0 +1,5 @@
+export const inc = () => ({type: 'INC'});
+export const dec = () => ({type: 'DEC'});
+export const rnd = () => {
+    return {type: 'INC', value: Math.floor(Math.random() * 10)}
+};
--- src/index.js
+++ src/index.js
@@ -1,58 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './components/app';
 
-const reducer = (state = 0, action) => { //must be clean function
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-    case 'DEC':
-      return state - 1;
-    case 'RES':
-      return 0;
-    default:
-      return state
-  }
-}
+const store = createStore(reducer);
 
-//creating the store
-let store = createStore(reducer);
-
-//subsidiary functions
-const inc = () => ({type: 'INC'}); //action-creators
-const dec = () => ({type: 'DEC'});
-const res = () => ({type: 'RES'})
-
-//click-buttons
-document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc())
-});
-
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec())
-});
-
-document.getElementById('res').addEventListener('click', () => {
-  store.dispatch(res())
-});
-
-//function updates counter
-const update = () => {
-  document.getElementById('counter').textContent = store.getState();
-}
-
-store.subscribe(update);
-
-/*store.subscribe(() => {
-  console.log(store.getState());
-})
-
-store.dispatch({type: 'INC'})
-store.dispatch({type: 'INC'})
-store.dispatch({type: 'INC'})*/
+ReactDOM.render(
+  <Provider store={store}> 
+    <App/>
+  </Provider>, document.getElementById('root'));
 
 
-/*let state = reducer(undefined, {});
-
-state = reducer(state, {type: 'INC'});
-console.log(state);
-state = reducer(state, {type: 'INC'});
-console.log(state);*/
